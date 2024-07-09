7f44f83395a24f557c9b8783b97586a98a453f7b
Kamaro <kamaroly@gmail.com>
2024-07-09 16:18:03 +0300

    Added test skeletons for select

---
+++ test/salad_ui/select_test.exs
@@ -0,0 +1,28 @@
+defmodule SaladUi.SelectTest do
+  use ComponentCase
+
+  import SaladUI.Select
+
+  describe "Test select component" do
+    test "It renders select_trigger correctly" do
+    end
+
+    test "It renders select_content correctly" do
+    end
+
+    test "It renders select_group correctly" do
+    end
+
+    test "It renders select_label correctly" do
+    end
+
+    test "It renders select_item correctly" do
+    end
+
+    test "It renders select_separator correctly" do
+    end
+
+    test "It renders select correclty" do
+    end
+  end
+end
